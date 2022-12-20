0fbb26716abd0997ea78e8f7406416a95b45740d
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-12-20 14:55:04 +0100

    added user context

--- front/src/index.js
+++ front/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { CurrentUserProvider } from './context/currentUser-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<CurrentUserProvider>
+				<App />
+			</CurrentUserProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
 );
