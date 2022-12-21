1ce8b36b230a1c3ccb0d5c0d909418146821c244
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-12-21 11:16:01 +0100

    added delete edit btns

--- front/src/components/Accounts/Accounts/Account.jsx
+++ front/src/components/Accounts/Accounts/Account.jsx
@@ -1,3 +1,9 @@
 export default function Account({ account }) {
-	return <li>{account.name}</li>;
+	return (
+		<li>
+			<p>{account.name}</p>
+			<button>edit</button>
+			<button>delete</button>
+		</li>
+	);
 }
