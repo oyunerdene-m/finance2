9d056c2f8082d2159dca6792c4983de0155d3c36
Delgermurun Purevkhuu <delgermurun@users.noreply.github.com>
2022-12-28 20:23:23 +0100

    new backend with sqlite database

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import session from 'express-session';
+import { User } from '@prisma/client';
 
 import apiRouter from './api';
-import { User, login, register } from './users';
+import { login, register } from './db/users';
 
 declare module 'express-session' {
-  export interface SessionData {
-    user: User;
-  }
+    export interface SessionData {
+        user: Pick<User, 'id' | 'name' | 'email'>;
+    }
 }
 
 const port = 4000;
@@ -15,94 +16,94 @@ const app = express();
 
 app.set('trust proxy', 1);
 app.use(
-  session({
-    name: 'finance-app.sid',
-    secret: process.env.SESSION_SECRET || 'finance-app.secret',
-    resave: false,
-    saveUninitialized: true,
-    // cookie: { secure: true },
-  }),
+    session({
+        name: 'finance-app.sid',
+        secret: process.env.SESSION_SECRET || 'finance-app.secret',
+        resave: false,
+        saveUninitialized: true,
+        // cookie: { secure: true },
+    }),
 );
 
 app.use(express.json());
 
 app.post('/api/v1/users/register', async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body;
-    const user = await register(name, email, password);
-    res.json({ data: { user } });
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const { name, email, password } = req.body;
+        const user = await register(name, email, password);
+        res.json({ data: { user } });
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.get('/api/v1/users/current-user', async (req, res, next) => {
-  try {
-    res.json({ data: { user: req.session.user || null } });
-  } catch (error) {
-    next(error);
-  }
+    try {
+        res.json({ data: { user: req.session.user || null } });
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.get('/api/v1/users/logout', async (req, res, next) => {
-  try {
-    if (req.session.user) {
-      req.session.user = undefined;
+    try {
+        if (req.session.user) {
+            req.session.user = undefined;
 
-      req.session.save(function (err) {
-        if (err) return next(err);
+            req.session.save(function (err) {
+                if (err) return next(err);
+                res.json({ data: { done: true } });
+            });
+            return;
+        }
+
         res.json({ data: { done: true } });
-      });
-      return;
+    } catch (error) {
+        next(error);
     }
-
-    res.json({ data: { done: true } });
-  } catch (error) {
-    next(error);
-  }
 });
 
 app.post('/api/v1/users/login', async (req, res, next) => {
-  try {
-    if (req.session.user) {
-      res.status(403).json({ error: 'Already logged in.' });
-      return;
+    try {
+        if (req.session.user) {
+            res.status(403).json({ error: 'Already logged in.' });
+            return;
+        }
+
+        const { email, password } = req.body;
+        const user = await login(email, password);
+
+        req.session.regenerate((err) => {
+            if (err) {
+                return next(err);
+            }
+
+            req.session.user = user;
+
+            req.session.save(function (err) {
+                if (err) return next(err);
+                res.json({ data: { user } });
+            });
+        });
+    } catch (error) {
+        next(error);
     }
-
-    const { email, password } = req.body;
-    const user = await login(email, password);
-
-    req.session.regenerate((err) => {
-      if (err) {
-        return next(err);
-      }
-
-      req.session.user = user;
-
-      req.session.save(function (err) {
-        if (err) return next(err);
-        res.json({ data: { user } });
-      });
-    });
-  } catch (error) {
-    next(error);
-  }
 });
 
 app.use('/api/v1', apiRouter);
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+    res.json({ message: 'Hello World!' });
 });
 
 const handleError: express.ErrorRequestHandler = (err, req, res, next) => {
-  console.error(err.stack);
+    console.error(err.stack);
 
-  res.json({ error: err.message || err.toString() });
+    res.json({ error: err.message || err.toString() });
 };
 
 app.use(handleError);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
---
+++ back/src/db/accounts.ts
@@ -0,0 +1,60 @@
+import { Account } from '@prisma/client';
+import { prisma } from '.';
+
+function getAccounts(userId: number, includeClosed: boolean = false) {
+    return prisma.account.findMany({
+        where: {
+            ownerId: userId,
+            isClosed: includeClosed ? undefined : false,
+        },
+    });
+}
+
+async function addAccount(userId: number, accountData: Omit<Account, 'id' | 'owner' | 'ownerId'>) {
+    const newAccount = await prisma.account.create({
+        data: {
+            ...accountData,
+            owner: { connect: { id: userId } },
+        },
+    });
+
+    return newAccount;
+}
+
+async function editAccount(userId: number, id: number, accountData: Account) {
+    const account = await prisma.account.findFirst({ where: { id, ownerId: userId }, select: { ownerId: true } });
+
+    if (!account || account.ownerId !== userId) {
+        throw new Error('Account not found');
+    }
+
+    const updatedAccount = await prisma.account.update({
+        where: { id },
+        data: {
+            ...account,
+            ...accountData,
+            ownerId: userId, // This is needed because the user can't change the owner
+        },
+    });
+
+    return updatedAccount;
+}
+
+async function closeAccount(userId: number, id: number) {
+    const account = await prisma.account.findFirst({ where: { id, ownerId: userId }, select: { ownerId: true } });
+
+    if (!account || account.ownerId !== userId) {
+        throw new Error('Account not found');
+    }
+
+    const updatedAccount = await prisma.account.update({
+        where: { id },
+        data: {
+            isClosed: true,
+        },
+    });
+
+    return updatedAccount;
+}
+
+export { Account, getAccounts, addAccount, editAccount, closeAccount };
